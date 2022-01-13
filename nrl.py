9066759e05f21803d58abb6d2ee17f766167298b
ZeNoonan <darragh.noonan@gmail.com>
2022-01-13 16:58:28 +0000

    update

--- nrl.py
+++ nrl.py
@@ -31,11 +31,14 @@ def read_csv_data_date(file):
 url = 'https://raw.githubusercontent.com/ZeNoonan/nrl/main/nrl_data.csv'
 # https://www.aussportsbetting.com/data/historical-nfl-results-and-odds-data/
 # team_names_id = read_csv_data('https://raw.githubusercontent.com/ZeNoonan/nrl/main/nrl_team_id.csv').copy()
-team_names_id = read_csv_data('C:/Users/Darragh/Documents/Python/nrl/nrl_data.csv').copy()
-
+# team_names_id = (read_csv_data('C:/Users/Darragh/Documents/Python/nrl/nrl_team_id.csv')).drop(['Unnamed: 0'],axis=1).copy()
+team_names_id = id_excel
+# st.write(team_names_id)
 # st.write(pd.read_csv(url))
 # data=pd.read_csv(url,parse_dates=['Date'])
+
 local='C:/Users/Darragh/Documents/Python/nrl/nrl_data.csv'
+
 # data=pd.read_csv(local,parse_dates=['Date'])
 data=(read_csv_data_date(local)).copy()
 # data=pd.read_csv(url,parse_dates=['Date'])
@@ -45,19 +48,175 @@ data=(read_csv_data_date(local)).copy()
 data['year']=data['Date'].dt.year
 data['month']=data['Date'].dt.month
 data['day']=data['Date'].dt.day
-data=data.drop(['Date'],axis=1)
+data=data.drop(['Date','Unnamed: 0'],axis=1)
 data['Date']=pd.to_datetime(data[['year','month','day']])
-
+data['Week'] = data['Week'].replace({'Finals':26})
+data['Week']=pd.to_numeric(data['Week'])
 team_names_id=team_names_id.rename(columns={'Team':'Home Team'})
-st.write(team_names_id)
-st.write(data)
+# st.write('original team id',team_names_id)
+# st.write(data)
 fb_ref_2020=pd.merge(data,team_names_id,on='Home Team').rename(columns={'ID':'Home ID'})
-# st.write(fb_ref_2020)
+# st.write('after merge',fb_ref_2020)
 team_names_id_2=team_names_id.rename(columns={'Home Team':'Away Team'})
+# st.write('team id 2',team_names_id_2)
 data=pd.merge(fb_ref_2020,team_names_id_2,on='Away Team').rename(columns={'ID':'Away ID','Home Score':'Home Points',
 'Away Score':'Away Points','Home Line Close':'Spread'})
 cols_to_move=['Week','Date','Home ID','Home Team','Away ID','Away Team','Spread']
 cols = cols_to_move + [col for col in data if col not in cols_to_move]
 data=data[cols]
 
-st.write(data)
+def spread_workings(data):
+    data['home_win']=data['Home Points'] - data['Away Points']
+    data['home_win'] = np.where((data['Home Points'] > data['Away Points']), 1, np.where((data['Home Points'] < data['Away Points']),-1,0))
+    data['home_cover']=(np.where(((data['Home Points'] + data['Spread']) > data['Away Points']), 1,
+    np.where(((data['Home Points']+ data['Spread']) < data['Away Points']),-1,0)))
+    data['home_cover']=data['home_cover'].astype(int)
+    data['away_cover'] = -data['home_cover']
+    data['Date']=pd.to_datetime(data['Date'])
+    # data=data.rename(columns={'Net Turnover':'home_turnover'})
+    # data['away_turnover'] = -data['home_turnover']
+    return data
+
+def turnover_workings(data,week_start):
+    turnover_df=data[data['Week']>week_start].copy()
+    turnover_df['home_turned_over_sign'] = np.where((turnover_df['Turnover'] > 0), 1, np.where((turnover_df['Turnover'] < 0),-1,0))
+    turnover_df['away_turned_over_sign'] = - turnover_df['home_turned_over_sign']
+    # season_cover_df=(data.set_index('Week').loc[week_start:,:]).reset_index()
+    home_turnover_df = (turnover_df.loc[:,['Week','Date','Home ID','home_turned_over_sign']]).rename(columns={'Home ID':'ID','home_turned_over_sign':'turned_over_sign'})
+    # st.write('checking home turnover section', home_turnover_df[home_turnover_df['ID']==0])
+    away_turnover_df = (turnover_df.loc[:,['Week','Date','Away ID','away_turned_over_sign']]).rename(columns={'Away ID':'ID','away_turned_over_sign':'turned_over_sign'})
+    # st.write('checking away turnover section', away_turnover_df[away_turnover_df['ID']==0])
+    season_cover=pd.concat([home_turnover_df,away_turnover_df],ignore_index=True)
+    # season_cover_df = pd.melt(season_cover_df,id_vars=['Week', 'home_cover'],value_vars=['Home ID', 'Away ID']).set_index('Week').rename(columns={'value':'ID'}).\
+    # drop('variable',axis=1).reset_index().sort_values(by=['Week','ID'],ascending=True)
+    return season_cover.sort_values(by=['Week','Date','ID'],ascending=['True','True','True'])
+
+def turnover_2(season_cover_df):    
+    # https://stackoverflow.com/questions/53335567/use-pandas-shift-within-a-group
+    season_cover_df['prev_turnover']=season_cover_df.groupby('ID')['turned_over_sign'].shift()
+    return season_cover_df.sort_values(by=['ID','Week'],ascending=True)
+    # return season_cover_df
+
+def season_cover_3(data,column_sign,name):
+    data[column_sign] = np.where((data[name] > 0), 1, np.where((data[name] < 0),-1,0))
+    return data
+
+def penalty_workings(data,week_start):
+    turnover_df=data[data['Week']>week_start].copy()
+    turnover_df['home_penalty_sign'] = np.where((turnover_df['penalties_conceded'] > 0), 1, np.where((turnover_df['penalties_conceded'] < 0),-1,0))
+    turnover_df['away_penalty_sign'] = - turnover_df['home_penalty_sign']
+    # season_cover_df=(data.set_index('Week').loc[week_start:,:]).reset_index()
+    home_turnover_df = (turnover_df.loc[:,['Week','Date','Home ID','home_penalty_sign']]).rename(columns={'Home ID':'ID','home_penalty_sign':'penalty_sign'})
+    # st.write('checking home turnover section', home_turnover_df[home_turnover_df['ID']==0])
+    away_turnover_df = (turnover_df.loc[:,['Week','Date','Away ID','away_penalty_sign']]).rename(columns={'Away ID':'ID','away_penalty_sign':'penalty_sign'})
+    # st.write('checking away turnover section', away_turnover_df[away_turnover_df['ID']==0])
+    season_cover=pd.concat([home_turnover_df,away_turnover_df],ignore_index=True)
+    # season_cover_df = pd.melt(season_cover_df,id_vars=['Week', 'home_cover'],value_vars=['Home ID', 'Away ID']).set_index('Week').rename(columns={'value':'ID'}).\
+    # drop('variable',axis=1).reset_index().sort_values(by=['Week','ID'],ascending=True)
+    return season_cover.sort_values(by=['Week','Date','ID'],ascending=['True','True','True'])
+
+def penalty_2(season_cover_df):    
+    # https://stackoverflow.com/questions/53335567/use-pandas-shift-within-a-group
+    season_cover_df['prev_penalty']=season_cover_df.groupby('ID')['penalty_sign'].shift()
+    return season_cover_df.sort_values(by=['ID','Week'],ascending=True)
+    # return season_cover_df
+
+def penalty_cover_3(data,column_sign,name):
+    data[column_sign] = np.where((data[name] > 0), 1, np.where((data[name] < 0),-1,0))
+    return data
+
+turnover=spread_workings(data)
+st.write('turnover workings', turnover)
+turnover_1 = turnover_workings(turnover,-1)
+turnover_2=turnover_2(turnover_1)
+turnover_3=season_cover_3(turnover_2,'turnover_sign','prev_turnover')
+
+penalty=spread_workings(data)
+# st.write('where is penalty??', penalty)
+penalty_1 = penalty_workings(penalty,-1)
+penalty_2=penalty_2(penalty_1)
+penalty_3=penalty_cover_3(penalty_2,'penalty_sign','prev_penalty')
+
+def season_cover_workings(data,home,away,name,week_start):
+    season_cover_df=data[data['Week']>week_start].copy()
+    # season_cover_df=(data.set_index('Week').loc[week_start:,:]).reset_index()
+    home_cover_df = (season_cover_df.loc[:,['Week','Date','Home ID',home]]).rename(columns={'Home ID':'ID',home:name})
+    # st.write('checking home turnover section', home_cover_df[home_cover_df['ID']==0])
+    away_cover_df = (season_cover_df.loc[:,['Week','Date','Away ID',away]]).rename(columns={'Away ID':'ID',away:name})
+    # st.write('checking away turnover section', away_cover_df[away_cover_df['ID']==0])
+    season_cover=pd.concat([home_cover_df,away_cover_df],ignore_index=True)
+    # season_cover_df = pd.melt(season_cover_df,id_vars=['Week', 'home_cover'],value_vars=['Home ID', 'Away ID']).set_index('Week').rename(columns={'value':'ID'}).\
+    # drop('variable',axis=1).reset_index().sort_values(by=['Week','ID'],ascending=True)
+    return season_cover.sort_values(by=['Week','Date','ID'],ascending=['True','True','True'])
+
+def season_cover_2(season_cover_df,column_name):    
+    # https://stackoverflow.com/questions/54993050/pandas-groupby-shift-and-cumulative-sum
+    # season_cover_df[column_name] = season_cover_df.groupby (['ID'])[column_name].transform(lambda x: x.cumsum().shift())
+    # THE ABOVE DIDN'T WORK IN 2020 PRO FOOTBALL BUT DID WORK IN 2019 DO NOT DELETE FOR INFO PURPOSES
+    season_cover_df[column_name] = season_cover_df.groupby (['ID'])[column_name].apply(lambda x: x.cumsum().shift())
+    season_cover_df=season_cover_df.reset_index().sort_values(by=['Week','Date','ID'],ascending=True).drop('index',axis=1)
+    # Be careful with this if you want full season, season to date cover, for week 17, it is season to date up to week 16
+    # if you want full season, you have to go up to week 18 to get the full 17 weeks, just if you want to do analysis on season covers
+    return season_cover_df
+
+spread=spread_workings(data)
+
+# with st.beta_expander('Season to date Cover'):
+spread_1 = season_cover_workings(spread,'home_cover','away_cover','cover',0)
+spread_2=season_cover_2(spread_1,'cover')
+spread_3=season_cover_3(spread_2,'cover_sign','cover')
+
+matrix_df=spread_workings(data)
+matrix_df=matrix_df.reset_index().rename(columns={'index':'unique_match_id'})
+test_df = matrix_df.copy()
+matrix_df['at_home'] = 1
+matrix_df['at_away'] = -1
+matrix_df['home_pts_adv'] = -3
+matrix_df['away_pts_adv'] = 3
+matrix_df['away_spread']=-matrix_df['Spread']
+matrix_df=matrix_df.rename(columns={'Spread':'home_spread'})
+matrix_df_1=matrix_df.loc[:,['unique_match_id','Week','Home ID','Away ID','at_home','at_away','home_spread','away_spread','home_pts_adv','away_pts_adv','Date','Home Points','Away Points']].copy()
+# st.write('checking matrix', matrix_df_1.dtypes)
+
+# with st.beta_expander('Games Played to be used in Matrix Multiplication'):
+first_qtr=matrix_df_1.copy()
+start=-3
+finish=0
+first_4=first_qtr[first_qtr['Week'].between(start,finish)].copy()
+def games_matrix_workings(first_4):
+    group_week = first_4.groupby('Week')
+    raw_data_2=[]
+    game_weights = iter([-0.125, -0.25,-0.5,-1])
+    for name, group in group_week:
+        group['game_adj']=next(game_weights)
+        raw_data_2.append(group)
+
+    df3 = pd.concat(raw_data_2, ignore_index=True)
+    adj_df3=df3.loc[:,['Home ID', 'Away ID', 'game_adj']].copy()
+    test_adj_df3 = adj_df3.rename(columns={'Home ID':'Away ID', 'Away ID':'Home ID'})
+    concat_df_test=pd.concat([adj_df3,test_adj_df3]).sort_values(by=['Home ID', 'game_adj'],ascending=[True,False])
+    test_concat_df_test=concat_df_test.groupby('Home ID')['game_adj'].sum().abs().reset_index()
+    test_concat_df_test['Away ID']=test_concat_df_test['Home ID']
+    full=pd.concat([concat_df_test,test_concat_df_test]).sort_values(by=['Home ID', 'game_adj'],ascending=[True,False])
+    full_stack=pd.pivot_table(full,index='Away ID', columns='Home ID',aggfunc='sum')
+    full_stack=full_stack.fillna(0)
+    full_stack.columns = full_stack.columns.droplevel(0)
+    return full_stack
+    
+    full_stack=games_matrix_workings(first_4)
+    # st.write('Check sum if True all good', full_stack.sum().sum()==0)
+
+# with st.beta_expander('CORRECT Testing reworking the DataFrame'):
+test_df['at_home'] = 1
+test_df['at_away'] = -1
+test_df['home_pts_adv'] = 3
+test_df['away_pts_adv'] = -3
+test_df['away_spread']=-test_df['Spread']
+test_df=test_df.rename(columns={'Spread':'home_spread'})
+test_df_1=test_df.loc[:,['unique_match_id','Week','Home ID','Away ID','at_home','at_away','home_spread','away_spread','home_pts_adv','away_pts_adv']].copy()
+test_df_home=test_df_1.loc[:,['Week','Home ID','at_home','home_spread','home_pts_adv']].rename(columns={'Home ID':'ID','at_home':'home','home_spread':'spread','home_pts_adv':'home_pts_adv'}).copy()
+test_df_away=test_df_1.loc[:,['Week','Away ID','at_away','away_spread','away_pts_adv']].rename(columns={'Away ID':'ID','at_away':'home','away_spread':'spread','away_pts_adv':'home_pts_adv'}).copy()
+test_df_2=pd.concat([test_df_home,test_df_away],ignore_index=True)
+test_df_2=test_df_2.sort_values(by=['ID','Week'],ascending=True)
+test_df_2['spread_with_home_adv']=test_df_2['spread']+test_df_2['home_pts_adv']
+# st.write(test_df_2)

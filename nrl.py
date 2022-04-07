16c233f1e21079d0f55e8d1d293725c7b2a9a793
ZeNoonan <darragh.noonan@gmail.com>
2022-04-07 16:59:38 +0100

    update

--- nrl.py
+++ nrl.py
@@ -9,9 +9,9 @@ st.set_page_config(layout="wide")
 
 # appears as if 2021 was normal year with normal home picks for the power pick factor
 # finished_week=26 # select this for 2021
-finished_week=4
+finished_week=5
 
-# all backed manly, sydney roosters, melbourne storm
+# all backed tue 5 april gold coast , canberra, canterbury
 
 placeholder_1=st.empty()
 placeholder_2=st.empty()
